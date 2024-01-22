d9d1ae3ce229d5402aeac03adaf103765110b23d
shyam1112 <shyamsardhara1112@gmail.com>
2024-01-22 14:39:45 +0530

    Add Arrow to on scroll bottom to top

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function openNav(){
 function closeNav(){
     document.getElementById("mySidenav").style.display="none";
 }
+
+// const toparrow1=document.getElementById("toparrow");
+// console.log(toparrow1);
+
+window.onscroll =function(){
+  scrollfunction();
+  // myarrow.style.display="block";
+  // return console.log(2+5);
+
+}
+
+function scrollfunction(){
+  if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+    document.getElementById("toparrow").style.display="block";
+  }else{
+    document.getElementById("toparrow").style.display="none";
+  }
+}
+
+function topfunction(){
+  document.body.scrollTop=0;
+  document.documentElement.scrollTop=0;
+}
